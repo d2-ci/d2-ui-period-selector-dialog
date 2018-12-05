46e1631a3842c6f22110b741f78a7475eaa738f0
Ilya Nee <neeilya@icloud.com>
2018-12-05 14:38:55 +0000

    ecc18ae Respect display name property in org units tree and options

---
+++ PeriodSelector.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+import Periods from './Periods';
+import Store from './reducers';
+
+var PeriodSelector = function PeriodSelector(props) {
+    return React.createElement(
+        Provider,
+        { store: Store },
+        React.createElement(Periods, props)
+    );
+};
+
+PeriodSelector.propTypes = {
+    onSelect: PropTypes.func,
+    onDeselect: PropTypes.func,
+    onReorder: PropTypes.func,
+    selectedItems: PropTypes.arrayOf(PropTypes.object)
+};
+
+PeriodSelector.defaultProps = {
+    onSelect: function onSelect() {
+        return null;
+    },
+    onDeselect: function onDeselect() {
+        return null;
+    },
+    onReorder: function onReorder() {
+        return null;
+    },
+    selectedItems: []
+};
+
+export default PeriodSelector;
